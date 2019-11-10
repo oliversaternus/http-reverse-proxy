a0a40a00d3abfd5dbfcabc257f9ef275dc94edad
oliversaternus <mail@oliversaternus.de>
2019-11-10 09:10:16 +0100

    websocket proxy

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 var https = require('https');
 var httpProxy = require('http-proxy');
 var fs = require('fs');
-var apiProxy = httpProxy.createProxyServer();
+var apiProxy = httpProxy.createProxyServer({ ws: true });
 const config = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf-8'));
 const logging = config.options.logging;
 const domains = config.domains;
@@ -52,6 +52,44 @@ const server = https.createServer({
     key: fs.readFileSync(__dirname + '/privkey.pem'),
     cert: fs.readFileSync(__dirname + '/fullchain.pem')
 }, proxyHandler);
+
+server.on("upgrade", function (req, res) {
+    try {
+        const host = req.headers.host;
+        if (!host) {
+            if (logging) {
+                console.log('{"ip": "' + req.connection.remoteAddress + '","address": "unknown", "event": "upgrade" }');
+            }
+            res.writeHead(404);
+            res.end();
+            return;
+        }
+        if (logging) {
+            console.log('{"ip": "' + req.connection.remoteAddress + '","host": "' + host + '","url":"' + req.url + '", "event": "upgrade"}');
+        }
+        for (let i = 0; i < domains.length; i++) {
+            if ((host + '') === domains[i].url) {
+                if (logging) {
+                    console.log("Websocket connection upgraded: " + domains[i].port);
+                }
+                apiProxy.ws(req, res, { target: 'http://localhost:' + domains[i].port }, function (e) {
+                    if (logging && e) {
+                        console.log(e);
+                    }
+                });
+                return;
+            }
+        }
+        res.writeHead(404);
+        res.end();
+    } catch (e) {
+        if (logging) {
+            console.log(e);
+        }
+        res.writeHead(500);
+        res.end();
+    }
+});
 
 server.listen(443, function () {
     console.log('proxy listening on port 443');
