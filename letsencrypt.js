d5b6d2b16a568c21ab5c2e5e78912eb4cc6a6d87
Oliver Saternus <mail@oliversaternus.de>
2020-07-24 12:43:36 +0200

    fix cronjob bug

--- letsencrypt.js
+++ letsencrypt.js
@@ -1,6 +1,6 @@
 const childProcess = require("child_process");
 const path = require("path");
-const CronJob = require("cron");
+const CronJob = require("cron").CronJob;
 const job = new CronJob("00 00 3 1 * *", function () {
     childProcess.exec("source ./letsencrypt.sh",
         { cwd: path.join(__dirname, "scripts"), shell: "/bin/bash" },
